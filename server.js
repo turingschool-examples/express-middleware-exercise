ed8ce015ae0b09c73411b1149383d1809e492f1e
Robbie Jaeger <jaeger.rob@gmail.com>
2018-07-06 07:44:00 -0600

    Add server endpoint for middleware application

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const bodyParser = require('body-parser');
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 
+// Create middleware function here --------->
+
+// ----------------------------------------->
+
+
+// Apply middleware function to this endpoint
+app.get('/api/v1/data', (request, response) => {
+
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Middleware exercise server running on http://localhost:${app.get('port')}`);
 });
