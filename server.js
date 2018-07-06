2c2c144a957b7b7ce746c0700a63ff39ea258a40
Robbie Jaeger <jaeger.rob@gmail.com>
2018-07-06 07:37:12 -0600

    Add body parser config to server file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
 
 app.listen(app.get('port'), () => {
   console.log(`Middleware exercise server running on http://localhost:${app.get('port')}`);
