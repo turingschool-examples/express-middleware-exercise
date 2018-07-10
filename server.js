8ad87d0195da9cdf0bfdc53e49e4cd46afa9b547
Robbie Jaeger <jaeger.rob@gmail.com>
2018-07-10 13:46:45 -0600

    Add finished code for lesson

--- server.js
+++ server.js
@@ -14,32 +14,42 @@ app.locals.pets = [{type: 'rabbit', name: 'Alan'}];
 // Log data about the request:
 // HTTP verb, endpoint, data in the request body, and anything else!
 
+const myLogger = (request, response, next) => {
+  console.log('Verb:', request.method, 'Endpoint:', request.url, 'Body:', request.body, 'Headers:', request.headers);
+  next();
+};
 
-
+app.use(myLogger);
 
 // 2. Second middleware function (apply to only endpoint POST /pets)
 // Check that there has been information sent in the request body
 // If there is a request body, continue to POST request handler
-// If there is not a request body, do not continue and send 422 response
+// If there is not data in the request body, do not continue and send 422 response
 
-
-
+const verifyPostBody = (request, response, next) => {
+  if (Object.keys(request.body).length === 0) {
+    response.status(422).send('Provide request body info...');
+  } else {
+    next();
+  }
+};
 
 // ---------------------------------------------------------------------->
 
 
 
 
-app.post('/api/v1/pets', (request, response) => {
+app.post('/api/v1/pets', verifyPostBody, (request, response) => {
   // Write functionality to add a pet to app.locals.pets and appropriate response
+  app.locals.pets.push(request.body);
+  response.status(201).json(request.body);
 });
 
 
 app.get('/api/v1/pets', (request, response) => {
   // Write functionality to return all pets in app.locals.pets
+  response.status(200).json(app.locals.pets);
 });
-
-
 
 
 app.listen(app.get('port'), () => {
