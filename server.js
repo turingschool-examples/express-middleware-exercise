233ab6b19799970910b3c30b690e1525691037b1
Travis Rollins <troll59910@aol.com>
2020-02-09 16:48:20 -0700

    Add new middleware for checking content-type

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors())
 
-app.locals.pets = [{type: 'rabbit', name: 'Alan'}];
+app.locals.pets = [
+  { name: 'Lassie', type: 'dog' },
+  { name: 'Felix', type: 'cat' },
+  { name: 'Garfield', type: 'cat' },
+  { name: 'Peter', type: 'rabbit' }
+];
 
 
 // Create middleware functions here ------------------------------------->
 
 // 1. First middleware function (apply this function to all endpoints)
 // Log data about the request:
-// HTTP verb, endpoint, data in the request body, and anything else!
+// HTTP verb, endpoint, data in the request body, and headers!
 
 const myLogger = (request, response, next) => {
   console.log('Verb:', request.method, 'Endpoint:', request.url, 'Body:', request.body, 'Headers:', request.headers);
@@ -28,26 +34,49 @@ app.use(myLogger);
 
 const verifyPostBody = (request, response, next) => {
   if (Object.keys(request.body).length === 0) {
-    return response.status(422).send('Provide request body info...');
-  } else {
-    next();
+    return response.status(422).send('Provide request body info.');
   }
+
+  next();
 };
+
+// 3. Third middleware function (apply to only endpoint POST /pets)
+// Check that the content-type is application/json
+// If the content-type is correct, continue to the next middleware
+// If the content-type is incorrect, do not continue and send 400 response
+
+const verifyJsonContent = (request, response, next) => {
+  if (request.headers['content-type'] !== 'application/json') {
+    return response.status(400).send('Server requires application/json')
+  }
+
+  next();
+}
 
 // ---------------------------------------------------------------------->
 
 
 
 
-app.post('/api/v1/pets', verifyPostBody, (request, response) => {
-  // Write functionality to add a pet to app.locals.pets and appropriate response
-  app.locals.pets.push(request.body);
-  response.status(201).json(request.body);
+app.post('/api/v1/pets', [verifyJsonContent, verifyPostBody], (request, response) => {
+  const pet = request.body;
+
+  for (let requiredParameter of ['name', 'type']) {
+    if (!pet[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, type: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  const { name, type } = pet;
+  app.locals.pets.push({ name, type });
+  response.status(201).json({ name, type });
+
 });
 
 
 app.get('/api/v1/pets', (request, response) => {
-  // Write functionality to return all pets in app.locals.pets
   response.status(200).json(app.locals.pets);
 });
 
